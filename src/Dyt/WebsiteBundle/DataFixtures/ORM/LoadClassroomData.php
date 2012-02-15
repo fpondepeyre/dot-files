0992154c446a1b6a4b0c572f1ce4678a83a18776
Florian <fpondepeyre@gmail.com>
2012-02-15 23:53:29 +0100

    [fixture] fix coding style. Refs #11

--- src/Dyt/WebsiteBundle/DataFixtures/ORM/LoadClassroomData.php
+++ src/Dyt/WebsiteBundle/DataFixtures/ORM/LoadClassroomData.php
@@ -9,6 +9,11 @@ use Dyt\WebsiteBundle\Entity\Classroom;
 
 class LoadClassroomData extends AbstractFixture implements OrderedFixtureInterface
 {
+    /**
+     * {@inheritdoc}
+     *
+     * @param ObjectManager $manager
+     */
     function load(ObjectManager $manager)
     {
         $classroom = new Classroom();
@@ -17,20 +22,24 @@ class LoadClassroomData extends AbstractFixture implements OrderedFixtureInterfa
 
         $manager->persist($classroom);
         $manager->flush();
-        
+
         $this->addReference('classroom1', $classroom);
-        
+
         $classroom = new Classroom();
         $classroom->setName('My classroom');
         $classroom->setSchool($manager->merge($this->getReference('laurent')));
 
         $manager->persist($classroom);
         $manager->flush();
-        
-        $this->addReference('classroom2', $classroom);
 
+        $this->addReference('classroom2', $classroom);
     }
-    
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return integer
+     */
     public function getOrder()
     {
         return 2;
--- src/Dyt/WebsiteBundle/DataFixtures/ORM/LoadStudentData.php
+++ src/Dyt/WebsiteBundle/DataFixtures/ORM/LoadStudentData.php
@@ -7,30 +7,101 @@ use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Dyt\WebsiteBundle\Entity\Student;
 
-class LoadStudentData extends AbstractFixture implements OrderedFixtureInterface {
+class LoadStudentData extends AbstractFixture implements OrderedFixtureInterface
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @param ObjectManager $manager
+     */
+    function load(ObjectManager $manager)
+    {
+        $students[] = array(
+            'firstname' => 'Tristan',
+            'lastname'  => 'RANA-COEUGNET',
+            'classroom' => 'classroom1',
+            'level'     => 'PS',
+            'sexe'      => 1,
+            'birthday'  => '2008-01-06'
+        );
 
-    function load(ObjectManager $manager) {
-        $students = array(
-            array('firstname' => 'Tristan', 'lastname' => 'RANA-COEUGNET', 'classroom' => 'classroom1',
-                'level' => 'PS', 'sexe' => 1, 'birthday' => '2008-01-06'),
-            array('firstname' => 'Anaé', 'lastname' => 'REIGNIER', 'classroom' => 'classroom1',
-                'level' => 'PS', 'sexe' => 2, 'birthday' => '2008-02-09'),
-            array('firstname' => 'Louise', 'lastname' => 'SEVRE', 'classroom' => 'classroom1',
-                'level' => 'MS', 'sexe' => 2, 'birthday' => '2007-12-21'),
-            array('firstname' => 'Henri', 'lastname' => 'JACQUEAU', 'classroom' => 'classroom1',
-                'level' => 'MS', 'sexe' => 1, 'birthday' => '2007-04-01'),
-            array('firstname' => 'Raphaël', 'lastname' => 'GALLOIS', 'classroom' => 'classroom1',
-                'level' => 'MS', 'sexe' => 1, 'birthday' => '2007-09-21'),
-            array('firstname' => 'Mattéo', 'lastname' => 'LIMA DA CUNHA', 'classroom' => 'classroom2',
-                'level' => 'MS', 'sexe' => 1, 'birthday' => '2007-04-19'),
-            array('firstname' => 'Léna', 'lastname' => 'DJERMANI', 'classroom' => 'classroom2',
-                'level' => 'MS', 'sexe' => 2, 'birthday' => '2007-06-12'),
-            array('firstname' => 'Héloïse', 'lastname' => 'ROGER', 'classroom' => 'classroom2',
-                'level' => 'TPS', 'sexe' => 2, 'birthday' => '2009-01-27'),
-            array('firstname' => 'Camille', 'lastname' => 'LANCIAUX', 'classroom' => 'classroom2',
-                'level' => 'TPS', 'sexe' => 2, 'birthday' => '2009-01-21'),
-            array('firstname' => 'Faustine', 'lastname' => 'CHENE', 'classroom' => 'classroom2',
-                'level' => 'PS', 'sexe' => 2, 'birthday' => '2008-10-23')
+        $students[] = array(
+            'firstname' => 'Anaé',
+            'lastname'  => 'REIGNIER',
+            'classroom' => 'classroom1',
+            'level'     => 'PS',
+            'sexe'      => 2,
+            'birthday'  => '2008-02-09'
+        );
+
+        $students[] = array(
+            'firstname' => 'Louise',
+            'lastname'  => 'SEVRE',
+            'classroom' => 'classroom1',
+            'level'     => 'MS',
+            'sexe'      => 2,
+            'birthday'  => '2007-12-21'
+        );
+
+        $students[] = array(
+            'firstname' => 'Henri',
+            'lastname'  => 'JACQUEAU',
+            'classroom' => 'classroom1',
+            'level'     => 'MS',
+            'sexe'      => 1,
+            'birthday'  => '2007-04-01'
+        );
+
+        $students[] = array(
+            'firstname' => 'Raphaël',
+            'lastname'  => 'GALLOIS',
+            'classroom' => 'classroom1',
+            'level'     => 'MS',
+            'sexe'      => 1,
+            'birthday'  => '2007-09-21'
+        );
+
+        $students[] = array(
+            'firstname' => 'Mattéo',
+            'lastname'  => 'LIMA DA CUNHA',
+            'classroom' => 'classroom2',
+            'level'     => 'MS', 'sexe' => 1,
+            'birthday'  => '2007-04-19'
+        );
+
+        $students[] = array(
+            'firstname' => 'Léna',
+            'lastname'  => 'DJERMANI',
+            'classroom' => 'classroom2',
+            'level'     => 'MS',
+            'sexe'      => 2,
+            'birthday'  => '2007-06-12'
+        );
+
+        $students[] = array(
+            'firstname' => 'Héloïse',
+            'lastname'  => 'ROGER',
+            'classroom' => 'classroom2',
+            'level'     => 'TPS',
+            'sexe'      => 2,
+            'birthday'  => '2009-01-27'
+        );
+
+        $students[] = array(
+            'firstname' => 'Camille',
+            'lastname'  => 'LANCIAUX',
+            'classroom' => 'classroom2',
+            'level'     => 'TPS',
+            'sexe'      => 2,
+            'birthday'  => '2009-01-21');
+
+        $students[] = array(
+            'firstname' => 'Faustine',
+            'lastname'  => 'CHENE',
+            'classroom' => 'classroom2',
+            'level'     => 'PS',
+            'sexe'      => 2,
+            'birthday'  => '2008-10-23'
         );
 
         $i = 0;
@@ -51,8 +122,13 @@ class LoadStudentData extends AbstractFixture implements OrderedFixtureInterface
         }
     }
 
-    public function getOrder() {
+    /**
+     * {@inheritdoc}
+     *
+     * @return integer
+     */
+    public function getOrder()
+    {
         return 4;
     }
-
 }
