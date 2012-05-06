18d23c600c9e61b6ab43822922cb08a8d7c6aa9c
Florian <fpondepeyre@gmail.com>
2012-05-06 15:33:06 +0200

    add schema.xml and model

---
+++ src/Dyt/WebsiteBundle/Model/Student.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Dyt\WebsiteBundle\Model;
+
+use Dyt\WebsiteBundle\Model\om\BaseStudent;
+
+
+/**
+ * Skeleton subclass for representing a row from the 'student' table.
+ *
+ * 
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ * @package    propel.generator.src.Dyt.WebsiteBundle.Model
+ */
+class Student extends BaseStudent {
+
+} // Student
