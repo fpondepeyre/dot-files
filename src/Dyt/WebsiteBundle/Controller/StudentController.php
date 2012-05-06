343f964f6ea1600243deba9153c69f3110593ed1
Florian <fpondepeyre@gmail.com>
2012-05-06 23:39:14 +0200

    update form classroom

--- src/Dyt/WebsiteBundle/Controller/StudentController.php
+++ src/Dyt/WebsiteBundle/Controller/StudentController.php
@@ -10,11 +10,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-use Dyt\WebsiteBundle\Form\StudentType;
 use Dyt\WebsiteBundle\Form\ClassroomType;
 
 use Dyt\WebsiteBundle\Model\StudentQuery;
-use Dyt\WebsiteBundle\Model\Student;
 use Dyt\WebsiteBundle\Model\ClassroomQuery;
 use Dyt\WebsiteBundle\Model\Classroom;
 
@@ -26,7 +24,7 @@ use Dyt\WebsiteBundle\Model\Classroom;
 class StudentController extends Controller
 {
     /**
-     * Lists all Student entities.
+     * Lists all students.
      *
      * @Route("/", name="student")
      * @Template()
@@ -38,111 +36,46 @@ class StudentController extends Controller
     }
 
     /**
-     * Finds and displays a Student entity.
-     *
-     * @Route("/{id}/show", name="student_show")
-     * @Template()
-     */
-    public function showAction($id)
-    {
-        $entity = StudentQuery::create()->findPk($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Student entity.');
-        }
-
-        $deleteForm = $this->createDeleteForm($id);
-
-        return array(
-            'entity'      => $entity,
-            'delete_form' => $deleteForm->createView()
-         );
-    }
-
-    /**
-     * Edits an existing Student entity.
-     *
-     * @Route("/{id}/update", name="student_update")
-     * @Method("post")
-     * @Template("DytWebsiteBundle:Student:edit.html.twig")
-     */
-    public function updateAction($id)
-    {
-        $entity = StudentQuery::create()->findPk($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Student entity.');
-        }
-
-        $editForm   = $this->createForm(new StudentType(), $entity);
-        $deleteForm = $this->createDeleteForm($id);
-
-        $request = $this->getRequest();
-
-        $editForm->bindRequest($request);
-
-        if ($editForm->isValid()) {
-            $em->persist($entity);
-            $em->flush();
-
-            return $this->redirect($this->generateUrl('student_edit', array('id' => $id)));
-        }
-
-        return array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        );
-    }
-
-    /**
-     * Deletes a Student entity.
-     *
-     * @Route("/{id}/delete", name="student_delete")
-     * @Method("post")
-     */
-    public function deleteAction($id)
-    {
-        $form = $this->createDeleteForm($id);
-        $request = $this->getRequest();
-
-        $form->bindRequest($request);
-
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getEntityManager();
-            $entity = StudentQuery::create()->findPk($id);
-
-            if (!$entity) {
-                throw $this->createNotFoundException('Unable to find Student entity.');
-            }
-
-            $em->delete();
-        }
-
-        return $this->redirect($this->generateUrl('student'));
-    }
-
-    private function createDeleteForm($id)
-    {
-        return $this->createFormBuilder(array('id' => $id))
-            ->add('id', 'hidden')
-            ->getForm()
-        ;
-    }
-
-    /**
-     * Displays a form to edit an existing Student entity.
+     * Displays a form to edit an existing classroom.
      *
      * @Route("/edit", name="student_edit")
      * @Template()
      */
     public function editAction(Request $request)
     {
-        $classroom = ClassroomQuery::create()
-            ->joinWith('Student')
-            ->findPk(1);
-
+        $classroom = ClassroomQuery::create()->findPk(12);
+        
         $form = $this->createForm(new ClassroomType(), $classroom);
+
+        return array(
+            'classroom' => $classroom,
+            'form'      => $form->createView()
+        );
+    }
+    
+    /**
+     * Edit an existing Student entity.
+     *
+     * @Route("/{id}/update", name="student_update")
+     * @Method("post")
+     * @Template("DytWebsiteBundle:Student:edit.html.twig")
+     */
+    public function updateAction($id, Request $request)
+    {          
+        $classroom = ClassroomQuery::create()->findPk($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find classroom entity.');
+        }
+        
+        $form = $this->createForm(new ClassroomType(), $classroom);
+        $form->bindRequest($request);
+
+        if ($form->isValid()) {
+            $classroom->save();
+            $this->get('session')->setFlash('notice', 'Your changes were saved!');
+            return $this->redirect($this->generateUrl('student', array()));
+        }
 
         return array(
             'form' => $form->createView()
